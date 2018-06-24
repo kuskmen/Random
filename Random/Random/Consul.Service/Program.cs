e045b268e3e37f2f75271902ee2465e458027113
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-06-24 23:56:01 +0300

    Selfhosted service that will be registered in consul and eventually be used for testing purposes.

---
+++ Random/Random/Consul.Service/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Consul.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(builder => builder.Map("/healthcheck", HealthCheckHandler))
+                .Build();
+
+           host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+
+        private static void HealthCheckHandler(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                context.Response.ContentType = context.Request.ContentType;
+                await context.Response.WriteAsync(
+                    JsonConvert.SerializeObject(new
+                    {
+                        StatusCode = context.Response.StatusCode.ToString(),
+                        PathBase = context.Request.PathBase.Value.Trim('/'),
+                        Path = context.Request.Path.Value.Trim('/'),
+                        Method = context.Request.Method,
+                        Scheme = context.Request.Scheme,
+                        ContentType = context.Request.ContentType,
+                        ContentLength = (long?)context.Request.ContentLength,
+                        Content = new StreamReader(context.Request.Body).ReadToEnd(),
+                        QueryString = context.Request.QueryString.ToString(),
+                        Query = context.Request.Query
+                            .ToDictionary(
+                                item => item.Key,
+                                item => item.Value,
+                                StringComparer.OrdinalIgnoreCase)
+                    })
+                );
+            });
+        }
+
+    }
+}
---
+++ Random/Random/Random/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Random
+{
+    public class JobScheduler
+    {
+        public JobScheduler(Job job)
+        {
+            
+        } 
+    }
+
+    public class Job
+    {
+        public Action Work { get; }
+
+        public string Id { get; }
+
+        public Job(Action work, string id)
+        {
+            Work = work;
+            Id = id;
+        }
+
+    }
+}
