b99ebe2f051327fe5851eaaf8bcfb228510f0f33
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-07-22 23:47:30 +0300

    Initial commit of Mandelbrot project.

---
+++ Mandelbrot/Drawing/Canvas.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace Drawing {
+    internal class Canvas : Panel {
+        private static readonly Canvas MyCanvas = new Canvas();
+
+        public Canvas() {
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            MyCanvas.BackColor = System.Drawing.Color.Transparent;
+
+        }
+
+        public void Draw(int x, int y, MouseEventArgs e, Graphics gr) {
+            var rect = new Rectangle(x, y, (int)(e.X - x), (int)(e.Y - y));
+            
+        }
+    }
+}
---
+++ Mandelbrot/Drawing/ComplexPoint.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Drawing {
+    /// <summary>
+    /// ComplexPoint class is used encapsulate a single complex point
+    /// Z = x + i*y where x and y are the real and imaginary parts respectively.
+    /// A number of complex arithmetic utility methods are provided.
+    /// </summary>
+    public struct ComplexPoint {
+        public double X;
+        public double Y;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="x">real part of complex point</param>
+        /// <param name="y">imaginary part of complex point</param>
+        public ComplexPoint(double x, double y) {
+            this.X = x;
+            this.Y = y;
+        }
+
+        /// <summary>
+        /// Calculate the modulus |Z| = Sqrt(x*x + y*y).
+        /// </summary>
+        /// <returns>Modulus of complex point</returns>
+        public double DoModulus() {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+
+        /// <summary>
+        /// Calculate modulus squared |Z|**2 = X*x + y*y.
+        /// </summary>
+        /// <returns>Modulus squared</returns>
+        public double DoMoulusSq() {
+            return X * X + Y * Y;
+        }
+
+        /// <summary>
+        /// Calculate complex square plus complex constant. The result
+        /// is another complex number.
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns>Z**2 + arg</returns>
+        public ComplexPoint DoCmplxSqPlusConst(ComplexPoint arg) {
+            var result = new ComplexPoint(0, 0)
+            {
+                X = X * X - Y * Y,
+                Y = 2 * X * Y
+            };
+            result.X += arg.X;
+            result.Y += arg.Y;
+            return result;
+        }
+    }
+}
